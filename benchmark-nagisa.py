75086a29e8e6f370d1bfd4529033be79d2af86b6
Paul O'Leary McCann <polm@dampfkraft.com>
2022-02-21 15:59:24 +0900

    Remove nagisa
    No updates since 2020.

--- benchmark-nagisa.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import nagisa
-from collections import Counter
-
-wc = Counter()
-
-for line in open('wagahai.txt'):
-    for word in nagisa.tagging(line.strip()).words:
-        wc[word] += 1
